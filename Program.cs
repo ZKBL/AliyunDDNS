7f8ad0ccbe26927b2a6fa45121e67fb6b5ccc441
ZKBL <flm919841090@gmail.com>
2020-03-11 15:03:53 +0800

    init

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace aliyunddns
+{
+    class Program
+    {
+        string getip
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
